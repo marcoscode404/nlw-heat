65a62318fb485eaf22fb03072cd3925b7b5080a6
Marcos Vini/trabalho <viniciusmg99@outlook.com>
2021-11-11 01:24:41 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+//lista de objetos
+const linksSocialMedia = {
+    github: 'marcoscode404',
+    youtube: 'Renato Cariani',
+    facebook: 'Marcos Vini Code',
+    instagram: 'marcosviniicode',
+    twitter: 'marcosssssssss'
+}
+
+
+function changeSocialMediaLinks() {
+    
+    for(let li of socialLinks.children){
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+
+        // alert(li.children[0].href)
+    }
+    
+
+// laço de repetição
+
+};
+//função
+
+changeSocialMediaLinks();
+//executa a função
+
+
+
+//criando função para ir no github pegar as informações e trazer para meu projeto
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        UserImage.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+    //fetch-->pega alguma informação função | se deu certo entre no then() ==> promessa de pegar alguma coisa
+}
+
+getGitHubProfileInfos();
+
+
+
+
+// arrow functions
